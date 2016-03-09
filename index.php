62d94b447731253845243df0be6bc3e1f1d462e0
sulcism <asdfrawr@gmail.com>
2016-03-08 17:28:17 -0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+
+include 'header.php';
+
+echo "
+	<h1>The Definitive 50</h1>
+	<br />
+	<a href=\"list.php?2\">N64 Games</a>
+	<a href=\"list.php?1\">SNES Games</a>
+	<a href=\"list.php?3\">GameCube Games</a>
+";
+
+?>
